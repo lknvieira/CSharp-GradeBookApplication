432cd5e8c585fba08d94b8024b705f211b51dfa1
lknvieira <lknvieira@gmail.com>
2021-09-25 21:22:22 -0300

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,8 +5,8 @@ namespace GradeBook.GradeBooks
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
-        {
+        {   
             Type = GradeBookType.Ranked;
-        }
+        }        
     }
 }
